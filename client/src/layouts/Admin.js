9afa77a0b467ddce433093999a926174f75efdea
ipekoztas <ipek.oztas@ug.bilkent.edu.tr>
2023-06-02 21:15:11 +0300

    adminnavbar and analysis page

---
+++ client/src/layouts/Admin.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import AdminNavBar from '../components/AdminNavBar';
+
+const AdminPage = () => {
+  return (
+    <div>
+      <AdminNavBar />
+      {/* Rest of your admin page content */}
+    </div>
+  );
+};
+
+export default AdminPage;
